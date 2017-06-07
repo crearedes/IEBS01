8646961537371326e73ce8adac7d36cca144ed91
crearedes <crea@crearedes.com>
2017-06-07 09:22:06 +0200

    iebs pruebas 01

---
+++ www/config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'iebs',
+		wp_ws_url : 'http://iebs.crearedes.es/wp-appkit-api/iebs',
+		wp_url : 'http://iebs.crearedes.es',
+		theme : 'wpak-theme-bootstrap-feature-user-login',
+		version : '',
+		app_title : 'IEBS',
+		app_platform : 'ios',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'Q/allo[e+Pt0VM<]Y/Eizh0T=X:J^@H`Sg}*17W_ccJ----C{3SFyE:w4e9Y|SqU',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
